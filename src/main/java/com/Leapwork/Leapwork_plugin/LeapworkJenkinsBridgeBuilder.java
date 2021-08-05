ffd5b3b2b0b9df3495b19897a5c4c58608895d30
BirSikanderMahajan <83980204+BirSikanderMahajan@users.noreply.github.com>
2021-08-05 18:09:31 +0530

    Requirements by Jenkins marketplace (#27)
    * Jenkins marketplace rules
    * Updating snapshot
    * Code refactoring

--- src/main/java/com/Leapwork/Leapwork_plugin/LeapworkJenkinsBridgeBuilder.java
+++ src/main/java/com/Leapwork/Leapwork_plugin/LeapworkJenkinsBridgeBuilder.java
@@ -51,7 +51,6 @@ public class LeapworkJenkinsBridgeBuilder extends Builder implements SimpleBuild
 	private String leapworkScheduleVariables;
 
 	private static PluginHandler pluginHandler = PluginHandler.getInstance();
-
 	// Fields in config.jelly must match the parameter names in the
 	// "DataBoundConstructor"
 	@DataBoundConstructor
@@ -180,8 +179,9 @@ public class LeapworkJenkinsBridgeBuilder extends Builder implements SimpleBuild
 				String schTitle = schedulesIdTitleHashMap.get(schId);
 				LeapworkRun run = new LeapworkRun(schId.toString(), schTitle);
 
-				UUID runId = pluginHandler.runSchedule(mainClient, controllerApiHttpAddress, Secret.toString(leapworkAccessKey), schId,
-						schTitle, listener, run, scheduleVariablesRequestPart);
+				UUID runId = pluginHandler.runSchedule(mainClient, controllerApiHttpAddress,
+						Secret.toString(leapworkAccessKey), schId, schTitle, listener, run,
+						scheduleVariablesRequestPart);
 				if (runId != null) {
 					resultsMap.put(runId, run);
 					CollectScheduleRunResults(controllerApiHttpAddress, Secret.toString(leapworkAccessKey), runId,
@@ -327,7 +327,7 @@ public class LeapworkJenkinsBridgeBuilder extends Builder implements SimpleBuild
 							resultRun.incFailed();
 						resultRun.incTotal();
 						break;
-
+					default:
 					}
 
 				}
--- src/main/java/com/Leapwork/Leapwork_plugin/model/LeapworkRun.java
+++ src/main/java/com/Leapwork/Leapwork_plugin/model/LeapworkRun.java
@@ -1,109 +1,176 @@
 package com.Leapwork.Leapwork_plugin.model;
 
-
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
 @XmlRootElement(name = "testsuite")
-public final class LeapworkRun
-{
-    private String scheduleTitle;
+public final class LeapworkRun {
+	private String scheduleTitle;
 
-    private String scheduleId;
+	private String scheduleId;
 
-    private String status;
+	private String status;
 
-    private int total;
+	private int total;
 
-    private int passed ;
+	private int passed;
 
-    private int failed ;
+	private int failed;
 
-    private int errors ;
+	private int errors;
 
-    private String error;
+	private String error;
 
-    private double time ;
+	private double time;
 
-    private int id;
+	private int id;
 
-    @XmlElement(name = "testcase")
-    public ArrayList<RunItem> runItems;
+	@XmlElement(name = "testcase")
+	public ArrayList<RunItem> runItems;
 
+	public LeapworkRun() {
+		runItems = new ArrayList<RunItem>();
+	}
 
-    public LeapworkRun() { runItems = new ArrayList<RunItem>(); }
-    public LeapworkRun(String title)
-    {
-        runItems = new ArrayList<RunItem>();
-        scheduleTitle = title;
-        failed = 0;
-        passed = 0;
-        errors = 0;
-        time = 0;
-    }
-    public LeapworkRun(String schid, String title)
-    {
-        runItems = new ArrayList<RunItem>();
-        scheduleId = schid;
-        scheduleTitle = title;
-        failed = 0;
-        passed = 0;
-        errors = 0;
-    }
+	public LeapworkRun(String title) {
+		runItems = new ArrayList<RunItem>();
+		scheduleTitle = title;
+		failed = 0;
+		passed = 0;
+		errors = 0;
+		time = 0;
+	}
 
-    @XmlAttribute(name = "name")
-    public String getScheduleTitle()            { return scheduleTitle;}
-    public void   setScheduleTitle(String name) { scheduleTitle = name; }
+	public LeapworkRun(String schid, String title) {
+		runItems = new ArrayList<RunItem>();
+		scheduleId = schid;
+		scheduleTitle = title;
+		failed = 0;
+		passed = 0;
+		errors = 0;
+	}
 
-    @XmlAttribute(name = "schId")
-    public String getScheduleId()               { return  scheduleId;}
-    public void   setScheduleId(String schId)   { scheduleId = schId; }
+	@XmlAttribute(name = "name")
+	public String getScheduleTitle() {
+		return scheduleTitle;
+	}
 
-    @XmlAttribute(name = "status")
-    public String getStatus()                   { return status; }
-    public void   setStatus(String status)      { this.status = status; }
+	public void setScheduleTitle(String name) {
+		scheduleTitle = name;
+	}
 
-    @XmlAttribute(name = "tests")
-    public int    getTotal()                    { return total; }
-    public void   setTotal(int tests)           { total = tests; }
-    public void   incTotal()                    { this.total++; }
+	@XmlAttribute(name = "schId")
+	public String getScheduleId() {
+		return scheduleId;
+	}
 
-    @XmlAttribute(name = "passed")
-    public int    getPassed()                   { return passed; }
-    public void   setPassed(int passed)         { this.passed = passed; }
-    public void   incPassed()                   { passed++; }
-    public void   addPassed(int passed)         { this.passed += passed; }
+	public void setScheduleId(String schId) {
+		scheduleId = schId;
+	}
 
-    @XmlAttribute(name = "failures")
-    public int    getFailed()                   { return failed; }
-    public void   setFailed(int failures)       { failed = failures; }
-    public void   incFailed()                   { failed++; }
-    public void   addFailed(int failures)       { this.failed += failures; }
+	@XmlAttribute(name = "status")
+	public String getStatus() {
+		return status;
+	}
 
-    @XmlAttribute(name = "errors")
-    public int    getErrors()                   { return errors; }
-    public void   setErrors(int errors)         { this.errors = errors; }
-    public void   incErrors()                   { errors++; }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    @XmlAttribute(name = "error")
-    public String getError()                    { return error; }
-    public void   setError(String error)
-    {
-        if(this.error == null)
-            this.error = error.replace("\n","&#xA;");
-        else
-            this.error += ("\n" + error).replace("\n","&#xA;");
-    }
+	@XmlAttribute(name = "tests")
+	public int getTotal() {
+		return total;
+	}
 
-    @XmlAttribute(name = "time")
-    public double getTime()                     { return time; }
-    public void   setTime(double time)          { this.time = time; }
-    public void   addTime(double time)          { this.time += time; }
+	public void setTotal(int tests) {
+		total = tests;
+	}
 
-    @XmlAttribute(name = "id")
-    public int    getId()                       { return id; }
-    public void   setId(int id)                 { this.id = id; }
+	public void incTotal() {
+		this.total++;
+	}
+
+	@XmlAttribute(name = "passed")
+	public int getPassed() {
+		return passed;
+	}
+
+	public void setPassed(int passed) {
+		this.passed = passed;
+	}
+
+	public void incPassed() {
+		passed++;
+	}
+
+	public void addPassed(int passed) {
+		this.passed += passed;
+	}
+
+	@XmlAttribute(name = "failures")
+	public int getFailed() {
+		return failed;
+	}
+
+	public void setFailed(int failures) {
+		failed = failures;
+	}
+
+	public void incFailed() {
+		failed++;
+	}
+
+	public void addFailed(int failures) {
+		this.failed += failures;
+	}
+
+	@XmlAttribute(name = "errors")
+	public int getErrors() {
+		return errors;
+	}
+
+	public void setErrors(int errors) {
+		this.errors = errors;
+	}
+
+	public void incErrors() {
+		errors++;
+	}
+
+	@XmlAttribute(name = "error")
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		if (this.error == null)
+			this.error = error.replace("\n", "&#xA;");
+		else
+			this.error += ("\n" + error).replace("\n", "&#xA;");
+	}
+
+	@XmlAttribute(name = "time")
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public void addTime(double time) {
+		this.time += time;
+	}
+
+	@XmlAttribute(name = "id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
